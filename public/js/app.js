382034bfc8fd2d22e74b66bf5500ad44cb115793
Alexandre Giraud <alexandre.giraud89@gmail.com>
2016-07-11 17:19:54 +0200

    Add routing

--- public/js/app.js
+++ public/js/app.js
@@ -1,2 +1,31 @@
 /*global angular*/
-var app = angular.module('generator', ['ngAnimate', 'ui.bootstrap', 'ui.bootstrap-slider']);
+var app = angular.module('generator', ['ngRoute', 'ngAnimate', 'ui.bootstrap', 'ui.bootstrap-slider']);
+
+
+app.config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+    'use strict';
+    $routeProvider
+        .when('/markovChain', {
+            templateUrl: '../views/markovChain.html',
+            controller: 'markovChainCtrl'
+        })
+        .when('/genericFantasy', {
+            templateUrl: '../views/fantasyNames.html',
+            controller: 'fantasyNamesCtrl'
+        })
+        .when('/fantasticSpecies', {
+            templateUrl: '../views/fantasticSpecies.html',
+            controller: 'fantasticSpeciesCtrl'
+        })
+        .when('/groups', {
+            templateUrl: '../views/groupNames.html',
+            controller: 'groupNamesCtrl'
+        })
+        .when('/taverns', {
+            templateUrl: '../views/taverns.html',
+            controller: 'tavernCtrl'
+        })
+        .otherwise({
+            redirectTo: '/markovChain'
+        });
+}]);
--- public/js/controllers/navCtrl.js
+++ public/js/controllers/navCtrl.js
@@ -2,6 +2,38 @@
 app.controller('navCtrl', ['$scope', '$window', function ($scope, $window) {
     'use strict';
 
-    $scope.isVertical = ($window.innerWidth < 768) ? true : false;
-    $scope.type = ($window.innerWidth < 768) ? 'pills' : 'tabs';
+    $scope.tabs = [
+        {
+            link: '#/markovChain',
+            label: 'Markov Chain'
+        },
+        {
+            link: '#/genericFantasy',
+            label: 'Generic Fantasy'
+        },
+        {
+            link: '#/fantasticSpecies',
+            label: 'Fantastic Species'
+        },
+        {
+            link: '#/groups',
+            label: 'Groups'
+        },
+        {
+            link: '#/taverns',
+            label: 'Taverns'
+        }
+    ];
+
+    $scope.selectedTab = $scope.tabs[0];
+    
+    $scope.onClickSelectTab = function (tab) {
+        $scope.selectedTab = tab;
+    };
+
+    $scope.tabClass = function (tab) {
+        return ($scope.selectedTab === tab) ? 'active' : '';
+    };
+
+    $scope.type = ($window.innerWidth < 768) ? 'nav-pills nav-stacked' : 'nav-tabs';
 }]);
